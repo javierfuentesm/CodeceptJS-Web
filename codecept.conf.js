0e2e79deab9e1b20a51e93d48e10ac0ea5461e44
Javier <javier.fuentesm@hotmail.com>
2021-02-05 10:29:01 -0600

    Se agrega paso para tomar captura de pantalla en cada paso

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,37 +1,41 @@
 exports.config = {
-  output: "./output",
-  helpers: {
-    Playwright: {
-      url: "https://www.saucedemo.com",
-      show: true,
-      browser: "chromium",
-    },
-  },
-  include: {
-    I: "./steps_file.js",
-    loginPage: "./pages/loginPage",
-  },
-  mocha: {},
-  bootstrap: null,
-  teardown: null,
-  hooks: [],
-  gherkin: {
-    features: "./features/*.feature",
-    steps: ["./step_definitions/steps.js", "./step_definitions/loginSteps.js"],
-  },
-  plugins: {
-    allure: {},
-    screenshotOnFail: {
-      enabled: true,
-    },
-    pauseOnFail: {},
-    retryFailedStep: {
-      enabled: true,
-    },
-    tryTo: {
-      enabled: true,
-    },
-  },
-  tests: "./*_test.js",
-  name: "framework-for-web",
-};
+	output: './output',
+	helpers: {
+		Playwright: {
+			url: 'https://www.saucedemo.com',
+			show: true,
+			browser: 'chromium',
+		},
+	},
+	include: {
+		I: './steps_file.js',
+		loginPage: './pages/loginPage',
+	},
+	mocha: {},
+	bootstrap: null,
+	teardown: null,
+	hooks: [],
+	gherkin: {
+		features: './features/*.feature',
+		steps: ['./step_definitions/steps.js', './step_definitions/loginSteps.js'],
+	},
+	plugins: {
+		allure: {},
+		screenshotOnFail: {
+			enabled: true,
+		},
+		pauseOnFail: {},
+		retryFailedStep: {
+			enabled: true,
+		},
+		tryTo: {
+			enabled: true,
+		},
+		stepByStepReport: {
+			enabled: false,
+			screenshotsForAllureReport: true,
+		},
+	},
+	tests: './*_test.js',
+	name: 'framework-for-web',
+}
