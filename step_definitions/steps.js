3a194f69d1f3143e5a56ea758f0067413004d598
Javier <javier.fuentesm@hotmail.com>
2021-04-13 17:39:43 -0500

    config eslint

--- step_definitions/steps.js
+++ step_definitions/steps.js
@@ -1,6 +1,6 @@
-const { I } = inject();
+const { I } = inject()
 // Add in your custom step files
 
 Given('I have a defined step', () => {
-  // TODO: replace with your own step
-});
+	// TODO: replace with your own step
+})
